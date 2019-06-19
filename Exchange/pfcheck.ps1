42b0cc92ec279d92301635a5d65c8ef6ebe8d001
GaryDCook <gcook@thecookshouse.com>
2019-06-18 17:21:44 -0700

    update pfcheck to include alias testing

--- Exchange/pfcheck.ps1
+++ Exchange/pfcheck.ps1
@@ -1,29 +1,35 @@
-﻿function test-pftext
+﻿$global:charfound = ""
+function test-pftext
 {
     param( [string]$Alias )
-   
+   $global:charfound = ""
     $IllegalAliasCharacters = 0..34+40..41+44,46+58..60+62+64+91..93+127..160+256
-
+    $return = $false
    Foreach ($c in $IllegalAliasCharacters) 
    {
        
         $char = [regex]::escape([char]$c)
+        
 
         if($Alias -match $char)
         {
             $return = $true
+            $global:charfound = "$($global:charfound)$($char)"
         }
-        else
-        {
-            $return = $false
-        }
+        
 
     }
        
     $return
 }
 write-host "Getting Public Folders"
-$pf = get-publicfolder-resultsize unlimited -recurse
+if ($pf -eq $null)
+{
+$pf = get-publicfolder -resultsize unlimited -recurse
+}
+else 
+{
+$pf = $pf}
 write-host "Processing public Folders"
 $results = @()
 $count = 0
@@ -35,27 +41,56 @@ foreach ($p in $pf)
     $obj = new-object psobject
     $obj | Add-Member -MemberType NoteProperty -Name PFidentity -Value $p.identity
     $obj | Add-Member -MemberType NoteProperty -Name PFName -Value $p.name
+    $nametest = test-pftext -Alias $p.name
+    if ($nametest -eq $true)
+    {
+    $obj | Add-Member -MemberType NoteProperty -Name Namevalid -Value $false
+    $obj | Add-Member -MemberType NoteProperty -Name Nameinvalidchar -Value ($global:charfound)
+    }
+    else
+    {
+    $obj | Add-Member -MemberType NoteProperty -Name Namevalid -Value $true
+    $obj | Add-Member -MemberType NoteProperty -Name Nameinvalidchar -Value ""
+    }
     $obj | Add-Member -MemberType NoteProperty -Name MailEnabled -Value $p.mailenabled
-    $obj | Add-Member -MemberType NoteProperty -Name Namevalid -Value (test-pftext -Alias $p.name)
-    $obj | Add-Member -MemberType NoteProperty -Name Identityvalid -Value (test-pftext -Alias $p.identity)
     if ($p.mailenabled -eq $true)
     {
         $mp = get-mailpublicfolder -identity $p.name | select *
         $obj | Add-Member -MemberType NoteProperty -Name alias -Value $mp.alias
-        $obj | Add-Member -MemberType NoteProperty -Name aliasvalid -Value (test-pftext -Alias $mp.alias)
-        $obj | Add-Member -MemberType NoteProperty -Name pemail -Value $mp.primarysmtpaddress
-        $obj | Add-Member -MemberType NoteProperty -Name pemailvalid -Value (test-pftext -Alias $mp.primarysmtpaddress)
+        $aliastest = test-pftext -Alias $mp.alias
+    if ($aliastest -eq $true)
+    {
+    $obj | Add-Member -MemberType NoteProperty -Name Aliasvalid -Value $false
+    $obj | Add-Member -MemberType NoteProperty -Name Aliasinvalidchar -Value ($global:charfound)
     }
     else
     {
-        $obj | Add-Member -MemberType NoteProperty -Name pemail -Value ""
-        $obj | Add-Member -MemberType NoteProperty -Name pemailvalid -Value ""
+    $obj | Add-Member -MemberType NoteProperty -Name Aliasvalid -Value $true
+    $obj | Add-Member -MemberType NoteProperty -Name Aliasinvalidchar -Value ""
+    }
+        $obj | Add-Member -MemberType NoteProperty -Name pemail -Value $mp.primarysmtpaddress
+        $pemailtest = test-pftext -Alias $mp.primarysmtpaddress
+    if ($pemailtest -eq $true)
+    {
+    $obj | Add-Member -MemberType NoteProperty -Name pemailvalid -Value $false
+    $obj | Add-Member -MemberType NoteProperty -Name pemailinvalidchar -Value ($global:charfound)
+    }
+    else
+    {
+    $obj | Add-Member -MemberType NoteProperty -Name pemailvalid -Value $true
+    $obj | Add-Member -MemberType NoteProperty -Name pemailinvalidchar -Value ""
+    }
+    }
+    else
+    {
+        
         $obj | Add-Member -MemberType NoteProperty -Name alias -Value ""
         $obj | Add-Member -MemberType NoteProperty -Name aliasvalid -Value ""
+        $obj | Add-Member -MemberType NoteProperty -Name pemail -Value ""
+        $obj | Add-Member -MemberType NoteProperty -Name pemailvalid -Value ""
     }
     $results += $obj
     $count += 1
 }
 write-host "Expoting File to current Directory"
 $results | export-csv -path ".\pfcheckexport.csv" -NoTypeInformation
-
